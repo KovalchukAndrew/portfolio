458c08f3c890b71a0467da5108da355f373cf293
KovalchukAndrew <dry.st.jim@gmail.com>
2021-11-13 23:28:07 +0300

    add-new-container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./header/Header";
-import Nav from "./nav/Nav";
+import Main from "./main/Main";
+
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Nav/>
+            <Main/>
+
         </div>
     );
 }
---
+++ src/main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Main.module.css'
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.greetingText}>
+                    <span>Hi there!</span>
+                    <h1>I am Andrew Kovalchuk</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Main;
