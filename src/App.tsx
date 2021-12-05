aeeba659db84c3d6bab2e953f41ca66ce665a92c
KovalchukAndrew <dry.st.jim@gmail.com>
2021-12-05 22:24:20 +0300

    skills-block

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Header from "./header/Header";
 import Main from "./main/Main";
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from "../common/style/Container.module.css";
+import Skill from "./Skill/Skill";
 
 
 function Skills() {
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.container}`}>
-                <h2>My skills</h2>
+                <h2 className={style.title}>My skills</h2>
                 <div className={style.skills}>
-
+                    <Skill title="JS"
+                           discription="Hard skills are usually teachable while soft skills are much harder to develop because they are typically personality traits and, therefore, extremely valuable to employers. In most cases, your soft skills can enhance your hard skills."/>
+                    <Skill title="React" discription="Hard skills are usually teachable while soft skills are much harder to develop because they are typically personality traits and, therefore, extremely valuable to employers. In most cases, your soft skills can enhance your hard skills."/>
+                    <Skill title="HTML/CSS" discription="Hard skills are usually teachable while soft skills are much harder to develop because they are typically personality traits and, therefore, extremely valuable to employers. In most cases, your soft skills can enhance your hard skills."/>
                 </div>
             </div>
 
